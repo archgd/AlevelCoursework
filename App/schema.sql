78de14e8459da9ad9c9ee91db55e9fcf5125ccff
archiehurrell <archgd912@outlook.com>
2024-04-02 07:22:39 +0100

    feat(schema.sql) created tables for custom workouts and preplanned workouts. foreign key added to outdoor workouts user(email)

--- App/schema.sql
+++ App/schema.sql
@@ -5,19 +5,36 @@ CREATE TABLE IF NOT EXISTS user
     forename VARCHAR(30) NOT NULL,
     surname  VARCHAR(30) NOT NULL,
     dob      VARCHAR(10) NOT NULL,
-    dob      VARCHAR(10) NOT NULL,
     PRIMARY KEY (email)
 );
 
 CREATE TABLE IF NOT EXISTS outdoorworkouts
 (
+
     ExerciseType VARCHAR(40) NOT NULL,
     Distance integer(4) NOT NULL,
     TimeTaken integer(4) NOT NULL,
     DateCompleted  VARCHAR(10) NOT NULL,
-    PRIMARY KEY (ExerciseType)
+    FOREIGN KEY (email) REFERENCES user(email)
+
+
+);
+
+CREATE TABLE IF NOT EXISTS gymworkouts
+(
+    Exercise VARCHAR(40) NOT NULL,
+    Reps integer(4) NOT NULL,
+    Weight integer(3) NOT NULL,
+    DateDone VARCHAR(10) NOT NULL,
+    FOREIGN KEY (email) REFERENCES user(email)
+
+
+
 )
 
-
-check
--- DROP table user;
+CREATE TABLE IF NOT EXISTS preplannedworkouts
+(
+    Workout VARCHAR(40) NOT NULL,
+    Date VARCHAR(10) NOT NULL,
+    FOREIGN KEY(email) REFERENCES user(email)
+)
